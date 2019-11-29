13f0b1e30f1fdeb2770f81ce97183288a6f01ff8
95lux <jlx@235media.com>
2019-11-29 17:48:02 +0100

    precache all images after api fetch

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,17 +1,19 @@
 import React from 'react'
-
+import Audioplayer from './Audioplayer'
 
 const userId = 288554452; // user_id of Ausschu$$war€
-const clientId = 'LPqZxMy7VDZTQ0w9g4fuCdMNflDZEdIs' // client_id of Ausschu$$war€
-//       client_id=ssV1Qfh3hGcBHMcuZ3bz3xHb3aoP5KUB
+// const clientId = 'LPqZxMy7VDZTQ0w9g4fuCdMNflDZEdIs' // client_id of Ausschu$$war€
+const clientId='jMtgnPXQjVKtkucQ61iCf5jKyDXGXxbS'
 var tracks = [];
+var images = [];
 var precached = [{
     id: 335383636,
     stream: 'https://api.soundcloud.com/tracks/335383636/stream?'+clientId,
-    artwork: 'https://i1.sndcdn.com/avatars-000684564896-2q8pvk-t500x500.jpg'
+    artwork: 'https://i1.sndcdn.com/avatars-000684564896-2q8pvk-t500x500.jpg',
+    title: 'wasgeht'
 }];
 var net = true;
-var tracksLength = 0;
+var tracksLength = null;
 
 
 
@@ -19,7 +21,12 @@ var tracksLength = 0;
 class Tracks extends React.Component {
 
     state = {
-        currentTrack: {}
+        currentTrack: {
+            id: null,
+            stream: '',
+            artwork: '',
+            title: ''
+        }
     }
 
     async loadFullData(url) {
@@ -44,24 +51,47 @@ class Tracks extends React.Component {
 
         let nextUrl = data.next_href;
         console.log(nextUrl);
-        if (nextUrl == undefined) {
-            tracksLength = tracksLength + tracksFetched.length -1;
+        if (nextUrl === undefined) {
+            tracks = Array.from(new Set(tracks.map(track => track.id)))
+                .map(id => {
+                    return tracks.find(a => a.id === id)
+                })
             this.shuffleArray(tracks)
 
-            // delete duplicates from tracks[]
-            const uniqueSet = new Set(tracks);
-            tracks = [...uniqueSet];
-
+            tracksLength = tracks.length
+            this.initialPreload();
+            // console.log(tracksLength);
+            // console.log(tracks);
             return;
         } else {
-            tracksLength = tracksLength + 50;
-            console.log(tracksFetched);
             this.loadFullData(nextUrl)
         }
     }
 
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    initialPreload(i) {
+            i = i || 0;
+            if (i <= tracksLength-1) {
+                let newPrecached = tracks[i];
+                images[i] = new Image();
+                images[i].src = newPrecached.artwork;
+                precached.push(newPrecached);
+                images[i].onload = () => {
+                    this.initialPreload(i+1);
+                }
+            } else {
+                console.log(precached);
+            }
+    }
+
     componentDidMount() {
-        let url = `https://api.soundcloud.com/users/${userId}/tracks?client_id=${clientId}&linked_partitioning=1&limit=50`
+        let url = `https://api.soundcloud.com/users/${userId}/tracks?client_id=${clientId}&linked_partitioning=1&limit=200`
         this.loadFullData(url);
 
         this.setState(prevState => ({
@@ -75,7 +105,7 @@ class Tracks extends React.Component {
         // this.changeTrack();
     }
 
-    changeTrack() {
+    changeTrack = () => {
         // let newTrack = this.state.tracks[Math.floor(Math.random() * this.state.tracks.length)];
         let newTrack = precached[Math.floor(Math.random() * precached.length)];
 
@@ -83,84 +113,27 @@ class Tracks extends React.Component {
             this.setState(prevState => ({
                 currentTrack: newTrack
             }));
-        // AUDIO FETCH
+
             net = false
         } else {
             net = true
         }
-        this.preloadTracks()
     }
 
-    // preloadTracks(i) {
-    //     i = i || 0;
-    //     if (precached.length == tracks.length) {
-    //         return;
-    //     }
-    //     if (i <= 5) {
-    //         var img = new Image ();
-    //
-    //         // img.onload = () => {
-    //         //     this.preloadTracks(i + 1);
-    //         // }
-    //         let newPrecached = tracks[Math.floor(Math.random() * tracks.length)];
-    //         if (precached.some(e => e.id === newPrecached.id) === true) {
-    //             this.preloadTracks(i + 1);
-    //             return;
-    //         } else {
-    //             img.src = newPrecached.artwork;
-    //             precached.push(newPrecached);
-    //         }
-    //         console.log(precached.length);
-    //         console.log(tracks.length);
-    //         // console.log(precached);
-    //     }
-    // }
-
-    preloadTracks(i) {
-        i = i || 0;
-
-        if (precached.length === tracks.length) {
-            return;
-        }
-        if (i <= 5) {
-            var img = new Image ();
-
-            // img.onload = () => {
-            //     this.preloadTracks(i + 1);
-            // }
-
-            let newPrecached = tracks[Math.floor(Math.random() * tracks.length)];
-            if (precached.some(e => e.id === newPrecached.id) === true) {
-                this.preloadTracks(i + 1);
-                return;
-            } else {
-                img.src = newPrecached.artwork;
-                precached.push(newPrecached);
-            }
-            console.log('precache[]'+precached.length);
-            console.log('tracks[]'+tracks.length);
-            // console.log(precached);
-        }
-    }
-
-    shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-
-    playPause() {
-        var audioElement = new Audio(this.state.currentTrack.stream);
-        console.log(this.state.currentTrack.stream);
-        audioElement.play();
+    playPause = () => {
+        let audio = new Audio(this.state.currentTrack.stream);
+        this.setState(prevState =>({
+            playing: !this.state.playing
+        }), () => {
+            this.state.playing ? audio.play() : audio.pause();
+        })
     }
 
     render () {
+        const { playing } = this.state;
         return (
             <div style={bgStyle}>
-                <div onClick={this.playPause.bind(this)} onMouseMove={this.changeTrack.bind(this)} onTouchMove={this.changeTrack.bind(this)} style={ sectionStyle }>
+                <div onClick={this.playPause} onMouseMove={this.changeTrack} onTouchMove={this.changeTrack} style={ sectionStyle }>
                     <img src={this.state.currentTrack.artwork} style={sectionStyle}></img>
                 </div>
             </div>
