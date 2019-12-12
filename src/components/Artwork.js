2d6efb835eaed9d33dc8009786a695bd96258ffd
95lux <jlx@235media.com>
2019-12-12 17:59:08 +0100

    put artwork rendering in seperate component

---
+++ src/components/Artwork.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Artwork extends React.Component {
+    state = {
+        artwork: ''
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.state.artwork === null) {
+            this.setState({ artwork: 'https://i1.sndcdn.com/avatars-000684564896-2q8pvk-t500x500.jpg'})
+        }
+        if (nextProps.currentTrackArtwork !== this.state.artwork) {
+            this.setState({ artwork: nextProps.currentTrackArtwork });
+        }
+
+    }
+    render () {
+
+
+        return(
+            <img src={this.state.artwork} style={sectionStyle}></img>
+        )
+    }
+}
+
+var sectionStyle = {
+    // backgroundImage: "url(" + backgroundUrl + ")",
+    width: "100vw",
+    height: "100vh",
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    position: 'center',
+    backgroundColor: 'slateblue'
+};
+
+export default Artwork;
